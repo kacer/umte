041b49d3fa650ffb8d9e96c05941112898355cf1
kacer <m.d.z.s@seznam.cz>
2019-03-25 15:57:18 +0100

    Add preparation code for eighth lesson - Sensors

--- app/src/main/java/cz/uhk/umteapp/sensors/SensorActivity.kt
+++ app/src/main/java/cz/uhk/umteapp/sensors/SensorActivity.kt
@@ -1,4 +1,37 @@
 package cz.uhk.umteapp.sensors
 
-class SensorActivity {
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cz.uhk.umteapp.R
+
+class SensorActivity : AppCompatActivity(), SensorEventListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensor)
+
+        // registrace k senzorum
+        val manager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        manager.registerListener(this, manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST)
+
+        /* TODO
+            - prava a definice aktivity v manifestu
+            - plnit xyzTextView hodnotami (formatovat)
+            - ukazat si rozdily senzoru
+            - orientation vector
+         */
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
